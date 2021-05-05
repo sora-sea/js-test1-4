30e6bc78aa792d0398a2359b06b3db037b0f74e2
sora-sea <yasu.abc.jda@gmail.com>
2021-05-05 09:05:57 +0900

    修正しました

--- js/main.js
+++ js/main.js
@@ -2,42 +2,40 @@
 
 {
   const title = [
-    "==============================",
-    "現在持っているタスクの一覧",
-    "=============================="
+    '==============================',
+    '現在持っているタスクの一覧',
+    '=============================='
     ];
 
   const task = [
-    {content: "机を片付ける", genre: "掃除",},
-    {content: "牛乳を買う", genre: "買い物",},
-    {content: "散歩する", genre: "運動",}
+    {content: '机を片付ける', genre: '掃除'},
+    {content: '牛乳を買う', genre: '買い物'},
+    {content: '散歩する', genre: '運動'}
   ];
 
-  title.forEach((titles) => {
-    console.log(`${titles}`)
-  });
+  function displayTasks() {
+    title.forEach((titles) => {
+      console.log(titles);
+    });
+  
+    task.forEach((value, index) => {
+      console.log(index + ' : ' + '[内容]' + task[index].content + '、' + '[ジャンル]' + task[index].genre);
+    });
+  }
 
-  task.forEach((value,index) => {
-    console.log(index + " : " + "[内容]" + task[index].content + "、" + "[ジャンル]" + task[index].genre)
-  });
+  displayTasks();
 
-  let newContent = prompt("タスクを入力してください");
-  let newGenre = prompt("ジャンルを入力してください");
+  const newContent = prompt('タスクを入力してください');
+  const newGenre = prompt('ジャンルを入力してください');
 
   task.push({
     content: newContent,
     genre: newGenre,
-  })
-
-  title.forEach((titles) => {
-    console.log(`${titles}`)
   });
 
-  task.forEach((value,index) => {
-    console.log(index + " : " + "[内容]" + task[index].content + "、" + "[ジャンル]" + task[index].genre)
-  });
+  displayTasks();
 
-  alert("新しいタスクを追加しました。");
+  alert('新しいタスクを追加しました。');
 
-  prompt("「確認,追加,削除,終了」のいずれかを入力してください");
+  prompt('「確認,追加,削除,終了」のいずれかを入力してください');
 }
